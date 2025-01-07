05eea5341e9ef5734123e752807a25b027d4f8b2
Gulshanshaik02 <161321135+shaikgulshan0205@users.noreply.github.com>
2025-01-07 10:04:16 +0530

    Add files via upload

---
+++ python ml.py
@@ -0,0 +1,297 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a="gulshan"
+type(a)
+
+
+# In[3]:
+
+
+b=33.3
+type(b)
+
+
+# In[6]:
+
+
+c=True
+type(c)
+
+
+# In[7]:
+
+
+d=22
+type(d)
+
+
+# In[8]:
+
+
+e=33+88j
+type(e)
+
+
+# ### addition +
+# ### subtratiion -
+# ### multiplication *
+# ### division /
+# ### remainder %
+# ### power **
+# ### // floor division
+
+# In[1]:
+
+
+a=109
+b=3
+
+
+# In[2]:
+
+
+c=a+b
+c
+
+
+# In[3]:
+
+
+type(c)
+
+
+# In[4]:
+
+
+d=a-b
+d
+
+
+# In[5]:
+
+
+type(d)
+
+
+# In[6]:
+
+
+d=a*b
+b
+
+
+# In[7]:
+
+
+type(d)
+
+
+# In[8]:
+
+
+d=a/b
+d
+
+
+# In[9]:
+
+
+type(d)
+
+
+# In[10]:
+
+
+d=a**b
+d
+
+
+# In[11]:
+
+
+type(d)
+
+
+# In[12]:
+
+
+d=a//b
+d
+
+
+# In[13]:
+
+
+type(d)
+
+
+# In[16]:
+
+
+d=a%b
+d
+
+
+# In[15]:
+
+
+type(d)
+
+
+# In[17]:
+
+
+a
+
+
+# In[18]:
+
+
+b
+
+
+# In[19]:
+
+
+a=45
+b=32
+
+
+# In[20]:
+
+
+a>b
+
+
+# In[21]:
+
+
+a<b
+
+
+# In[22]:
+
+
+a>=b
+
+
+# In[23]:
+
+
+a<=b
+
+
+# In[24]:
+
+
+a==b
+
+
+# In[25]:
+
+
+a !=b
+
+
+# ### logical operators
+# 
+
+# and 
+# or
+# not
+
+# In[27]:
+
+
+a=50 
+b=45 
+
+
+# In[28]:
+
+
+(a > b)
+
+
+# In[29]:
+
+
+a>100
+
+
+# In[30]:
+
+
+(a>b) and (a>100)
+
+
+# In[31]:
+
+
+(a>b) or (a>100)
+
+
+# In[34]:
+
+
+d=20
+d
+
+
+# In[35]:
+
+
+d<30
+
+
+# In[36]:
+
+
+d>30
+
+
+# In[37]:
+
+
+d>=10 and d<=30
+
+
+# In[38]:
+
+
+d=8.4
+
+
+# In[39]:
+
+
+d>=8.1 or d<=.0
+
+
+# In[40]:
+
+
+a>b
+
+
+# In[41]:
+
+
+cgpa=8.9
+
+
+# In[42]:
+
+
+cgpa>=8.1 and cgpa<=9.0 # A grade 
+
+
+# In[44]:
+
+
+cgpa>=7.1 and cgpa<=9.0 ## B grade
+
