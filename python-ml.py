885c9dc53369374812847b929590a8321485907f
Gulshanshaik <161321135+shaikgulshan0205@users.noreply.github.com>
2025-01-21 10:19:57 +0530

    Add files via upload

---
+++ python-ml.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as np
+import seaborn as sns
+
+
+# In[2]:
+
+
+sns.get_dataset_names()
+
+
+# In[3]:
+
+
+sns.load_dataset('iris')
+
+
+# In[5]:
+
+
+df('payment').unique()
+
+
+# In[6]:
+
+
+a=pd.Series(3,4,5,6,7,8,9)
+
+
+# In[9]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+a=pd.Series(3,4,5,6,7,8)
+
+
+# In[ ]:
+
+
+
+
